bde4c07d741ff248a1e3163705753532abab4808
Yara <yaramuhammad762@gmail.com>
2024-07-01 02:23:46 +0300

    d

--- des/lib/Components/Drawer.dart
+++ des/lib/Components/Drawer.dart
@@ -1,14 +1,15 @@
+import 'package:des/Screens/Profile/Profile.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../Controllers/AuthController.dart';
 import '../Models/user.dart';
 import '../Providers/UserProvider.dart';
-import '../Screens/Profile/Profile.dart';
 import '../constants.dart' as constants;
+import 'package:des/Controllers/AuthController.dart';
+
 import 'ProfilePhoto.dart';
 
-class MyDrawer extends StatelessWidget {
-  const MyDrawer({Key? key});
+class myDrawer extends StatelessWidget {
+  const myDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,71 +17,68 @@ class MyDrawer extends StatelessWidget {
         Provider.of<UserProvider>(context, listen: false);
     User? currentUser = userProvider.user;
     return Drawer(
-      child: Container(
-        color: constants.pageColor, // Set your desired background color
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              decoration: BoxDecoration(
-                color: constants.pageColor,
-              ),
-              accountName: Text(
-                '${currentUser?.firstName ?? ''} ${currentUser?.lastName ?? ''}',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                ),
-              ),
-              accountEmail: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Profile()),
-                  );
-                },
-                child: Text(
-                  'view profile →',
-                  style: TextStyle(
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: getProfilePhoto(context),
+      child: ListView(
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            decoration: BoxDecoration(
+              color: constants.pageColor,
+            ),
+            accountName: Text(
+              '${currentUser?.firstName ?? ''} ${currentUser?.lastName ?? ''}',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
               ),
             ),
-            ListTile(
-              leading: Icon(
-                Icons.notifications,
-                size: 24.0,
-                color: Colors.black,
-              ),
-              title: Text('Notifications'),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.settings,
-                size: 24.0,
-                color: Colors.black,
-              ),
-              title: Text('Preferences'),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.logout,
-                size: 24.0,
-                color: Colors.black,
-              ),
-              title: Text('Logout'),
-              onTap: () async {
-                logout(context);
+            accountEmail: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => (Profile())),
+                );
               },
+              child: Text(
+                'view profile →',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
             ),
-          ],
-        ),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: getProfilePhoto(context),
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.notifications,
+              size: 24.0,
+              color: Colors.black,
+            ),
+            title: Text('Notifications'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.settings,
+              size: 24.0,
+              color: Colors.black,
+            ),
+            title: Text('Prefernces'),
+            onTap: () {},
+          ),
+          Spacer(),
+          ListTile(
+            leading: Icon(
+              Icons.logout,
+              size: 24.0,
+              color: Colors.black,
+            ),
+            title: Text('Logout'),
+            onTap: () async {
+              logout(context);
+            },
+          ),
+        ],
       ),
     );
   }
